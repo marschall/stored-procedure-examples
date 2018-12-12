ffc179f6b98322b3573b6f7fb172781ec38c10ca
Philippe Marschall <philippe.marschall@gmail.com>
2018-12-12 17:28:23 +0100

    Bump broken Cayenne sample

---
+++ src/test/java/com/github/marschall/storedprocedureexamples/CayenneTest.java
@@ -0,0 +1,40 @@
+package com.github.marschall.storedprocedureexamples;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.QueryResponse;
+import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.query.ProcedureQuery;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+
+@Disabled("broken")
+public class CayenneTest extends AbstractExampleTest {
+  
+//  private DataContext context;
+  private ObjectContext context;
+
+  @BeforeEach
+  void setUp() {
+    ServerRuntime cayenneRuntime = ServerRuntime.builder()
+        //        .addConfig("cayenne-project.xml")
+        .dataSource(getDataSource())
+        .build();
+    this.context = cayenneRuntime.newContext();
+  }
+
+  @Override
+  protected int plus1inout(int arg) throws SQLException {
+    
+    ProcedureQuery query = new ProcedureQuery("plus1inout");
+
+    query.addParameter("arg", arg);
+    
+    QueryResponse result = context.performGenericQuery(query);
+    List outList = result.firstList();
+    return (int) outList.get(0);
+  }
+
+}
